b99b4f6c429bb4f0d3ca73003fc8072cfae7b0ff
Borodi Bogdan <bogdanborodiv@gmail.com>
2024-02-22 17:31:26 +0200

    message

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,21 @@
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 export function Banner() {
+    const [text] = useTypewriter({
+        words: ["Borodi Bogdan!", "Bob!", "a Software Developer!"],
+        loop: {},
+        typeSpeed: 120,
+        deleteSpeed: 80
+    })
     return (
         <section id="home" className="banner">
             <Container className="hero-container">
                 <Row className="add-line">
                     <Col className="col">
-                        <h1>Hi, I'm Borodi Bogdan!</h1>
+                        <h1>Hi, I'm <span>{text}</span><Cursor /></h1>
                         <p>Greetings! My name is <strong>Borodi Bogdan
                         </strong>, but my closest friends call me <strong>Bob
                             </strong>. I am a 19-year-old  student from <strong>Bistrita, Romania
