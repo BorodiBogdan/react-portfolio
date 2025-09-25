f40f84168037fadaf8c9c7a846925b7137055036
BorodiBogdan <bogdanborodiv@gmail.com>
2025-09-25 21:25:15 +0300

    Edited about me details

--- src/assets/records/about-images.jsx
+++ src/assets/records/about-images.jsx
@@ -2,7 +2,7 @@ const IMAGES = {
   0: require("../img/poza-eu.jpg"),
   1: require("../img/education.jpg"),
   2: require("../img/QPillars-logo.png"),
-  3: require("../img/microsoft.avif"),
+  3: require("../img/microsoft.jpeg"),
   4: require("../img/moisil.jpg"),
   5: require("../img/tutoring.png"),
 };
